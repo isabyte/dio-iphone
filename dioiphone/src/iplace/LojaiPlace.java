3c84f2590a41adc415d426ddbabb8f603223119c
Isabela Migliorini <isabelamigli@gmail.com>
2024-06-05 15:44:54 -0300

    implementação método main

---
+++ dioiphone/src/iplace/LojaiPlace.java
@@ -0,0 +1,20 @@
+package iplace;
+
+import iphone.CelulariPhone;
+
+public class LojaiPlace {
+
+	public static void main(String[] args) {
+		CelulariPhone iPhone = new CelulariPhone();
+		
+		iPhone.ligar();
+		
+		iPhone.atender();
+		
+		iPhone.adicionarNovaAba();
+		
+		iPhone.tocar();
+
+	}
+
+}
